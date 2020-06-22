938a1bbfb9312a96b24c4610530aec861da5ea25
ArekkuusuJerii <edgargcx99@gmail.com>
2020-06-22 18:47:06 -0500

    Fix bug with Non-human entities not getting targeted when the default team is off

--- src/main/java/arekkuusu/enderskills/api/helper/TeamHelper.java
+++ src/main/java/arekkuusu/enderskills/api/helper/TeamHelper.java
@@ -18,7 +18,7 @@ public class TeamHelper {
     public static final Function<Entity, Predicate<Entity>> NOT_SAME_TEAM = e -> e != null ? EntitySelectors.getTeamCollisionPredicate(e) : Predicates.alwaysFalse();
     public static final Predicate<Entity> NOT_CREATIVE = entity -> !(entity instanceof EntityPlayer) || !((EntityPlayer) entity).capabilities.isCreativeMode;
     public static final Predicate<Entity> HUMAN_TEAM = entity -> !EnderSkillsAPI.defaultHumanTeam || !entity.isCreatureType(EnumCreatureType.MONSTER, false);
-    public static final Predicate<Entity> NOT_HUMAN_TEAM = entity -> EnderSkillsAPI.defaultHumanTeam &&  entity.isCreatureType(EnumCreatureType.MONSTER, false);
+    public static final Predicate<Entity> NOT_HUMAN_TEAM = entity -> !EnderSkillsAPI.defaultHumanTeam || entity.isCreatureType(EnumCreatureType.MONSTER, false);
     public static final Function<Entity, Predicate<Entity>> SELECTOR_ALLY = (e) -> Predicates.or(Predicates.and(SAME_TEAM.apply(e), NOT_CREATIVE, HUMAN_TEAM), input -> input == e);
     public static final Function<Entity, Predicate<Entity>> SELECTOR_ENEMY = (e) -> Predicates.and(NOT_SAME_TEAM.apply(e), NOT_CREATIVE, NOT_HUMAN_TEAM, input -> input != e);
 
