b12ab989cd5d4f80042a556e9affd46ca6615c2f
ArekkuusuJerii <edgargcx99@gmail.com>
2020-07-05 19:55:51 -0500

    Update version number

--- src/main/java/arekkuusu/enderskills/api/package-info.java
+++ src/main/java/arekkuusu/enderskills/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "enderskills", apiVersion = "2", provides = "EnderSkillsAPI")
+@API(owner = "enderskills", apiVersion = "4", provides = "EnderSkillsAPI")
 package arekkuusu.enderskills.api;
 
 import net.minecraftforge.fml.common.API;
